bd32c821dc1e23d5d081991257bf26b965a57481
KanadeAditya <110182805+KanadeAditya@users.noreply.github.com>
2023-07-19 09:47:36 +0530

    Initial commit

---
+++ app/components/Navbar/Navbar.tsx
@@ -0,0 +1,20 @@
+import React from 'react'
+import Container from '../Container'
+
+function Navbar() {
+  return (
+    <div className='fixed w-full bg-[#55bcc9] z-10 shadow-sm'>
+        <div
+        className='
+            py-4
+            border-b-[1px]
+        '
+        >
+
+        </div>
+        <Container></Container>
+    </div>
+  )
+}
+
+export default Navbar
